2dcf420d5f7978cc7d5994fb843057280f6e728e
Andri Oskarsson <ano@venuepos.net>
2021-04-12 15:38:43 +0200

    playing around

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
+  app.on("issues.edited", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Tak for edit. Du er sød.",
     });
     await context.octokit.issues.createComment(issueComment);
+  });
+
+  app.on("release.created", async (context) => {
+    /**
+     * 1. List releases and find the latest release (lower semver)
+     * 2. Find this release
+     * 3. Do a compare to find all pull-requests merged in between
+     * 4. Generate release draft
+     * 5. Add to this release
+     */
+
+    //const latestRelease = await context.octokit.repos.getLatestRelease();
+    //const releaseId = context.payload.release.id
+    //const releaseOwner = context.payload.release.author
+    //const releaseRepo = context.payload.repository
+    context.log.info("debug", context.payload)
+
+    //context.octokit.repos.getRelease(context.payload.release.id)
+    return;
   });
   // For more information on building apps:
   // https://probot.github.io/docs/
